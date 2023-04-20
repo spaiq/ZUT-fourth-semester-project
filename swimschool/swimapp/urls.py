c81d3900b6aa43031edff1a72a92530c8185ad61
Bartłomiej Juszczyk <jb50996@zut.edu.pl>
2023-04-20 15:14:20 +0200

    feat: initial commit

---
+++ swimschool/swimapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+    # path('about/', views.about, name='about'),
+    # path('calendar/', views.calendar, name='calendar'),
+    # path('contact/', views.contact, name='contact'),
+    # path('faq/', views.faq, name='faq'),
+    # path('instructors/', views.instructors, name='instructors'),
+    # path('bookkeeping/', views.bookkeeping, name='bookkeeping'),
+]
