9bdb0459800bfb4064ede916aef9e27cc1dfeaca
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-06-13 18:24:12 +0200

    refactor: make the code "blackened" using black formatter

--- swimschool/swimapp/apps.py
+++ swimschool/swimapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SwimappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'swimapp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "swimapp"
