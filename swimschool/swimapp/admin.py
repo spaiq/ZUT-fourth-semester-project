23b4e26ba3a0e512698d16034e39a025fbd73ec2
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-06-13 18:14:44 +0200

    feat: register models

--- swimschool/swimapp/admin.py
+++ swimschool/swimapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Availability)
+admin.site.register(Instructor)
+admin.site.register(Swimmer)
+admin.site.register(Group)
+admin.site.register(Calendar)
+admin.site.register(Payment)
