2a59246763bf344acf85f7d8325d6d8762c8addd
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-06-20 16:23:55 +0200

    feat: add availibility not to be required

--- swimschool/swimapp/models.py
+++ swimschool/swimapp/models.py
@@ -14,7 +14,7 @@ class Instructor(models.Model):
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
     email = models.EmailField(max_length=100)
-    availability = models.ManyToManyField(Availability)
+    availability = models.ManyToManyField(Availability, blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
