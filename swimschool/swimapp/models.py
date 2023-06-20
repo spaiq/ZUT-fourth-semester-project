6210c5e01f5aa1870ffda84b1b793b02b3a161a7
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-06-20 22:35:55 +0200

    feat: add functionality to create empty groups

--- swimschool/swimapp/models.py
+++ swimschool/swimapp/models.py
@@ -25,7 +25,7 @@ class Swimmer(models.Model):
 class Group(models.Model):
     level = models.CharField(max_length=100)
     instructor = models.ForeignKey(User, on_delete=models.CASCADE)
-    swimmer = models.ManyToManyField(Swimmer)
+    swimmer = models.ManyToManyField(Swimmer, blank=True)
 
     def __str__(self):
         return (
