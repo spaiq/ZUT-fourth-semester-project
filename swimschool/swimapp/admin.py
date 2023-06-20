ae1085d86c3a834d33ea071753aa4124eb123703
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-06-20 19:13:05 +0200

    fix: delete instructor model register

--- swimschool/swimapp/admin.py
+++ swimschool/swimapp/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Availability)
-admin.site.register(Instructor)
 admin.site.register(Swimmer)
 admin.site.register(Group)
 admin.site.register(Lesson)
