e68ed4629dd9491b6267fd0827e35ae276edb6da
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-06-21 16:25:35 +0200

    feat: Gamma 1.0

--- swimschool/swimapp/urls.py
+++ swimschool/swimapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
@@ -8,9 +9,8 @@ urlpatterns = [
     path("calendar/create/", LessonCreateView.as_view()),
     path("manage/calendar/", manage_calendar, name="calendar"),
     path("manage/calendar/<int:pk>", LessonView.as_view()),
-    path("groups/", GroupView.as_view(), name="groups"),
     path("groups/add", AddGroupView.as_view(), name="add_manage_groups"),
-    path("manage/groups/", manage_groups, name="manage_groups"),
+    path("manage/groups/", GroupView.as_view(), name="groups"),
     path("manage/groups/<int:pk>", SingleGroupView.as_view()),
     path("contact/", contact, name="contact"),
     path("manage/", manage, name="manage"),
@@ -18,14 +18,13 @@ urlpatterns = [
     path("instructors/", instructors, name="instructors"),
     path("payments/", PaymentsView.as_view(), name="payments"),
     path("payments/<int:pk>", SinglePaymentView.as_view(), name="payments"),
-
-    path("manage-instructors/", InstructorView.as_view(), name="manage-instructors"),
+    path("manage/instructors/", InstructorView.as_view(), name="manage-instructors"),
     path(
         "manage-instructors/<int:pk>",
         SingleInstructorView.as_view(),
         name="manage-instructors",
     ),
-    path("manage-bookkeepers/", BookkeeperView.as_view(), name="manage-bookkeepers"),
+    path("manage/bookkeepers/", BookkeeperView.as_view(), name="manage-bookkeepers"),
     path(
         "manage-bookkeepers/<int:pk>",
         SingleBookkeeperView.as_view(),
@@ -34,4 +33,3 @@ urlpatterns = [
     path("accounts/login/", login_view, name="login"),
     path("accounts/logout/", logout_view, name="logout"),
 ]
-
